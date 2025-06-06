d58e8946e79d708a310289558cc7cdd6c8f71848
yuyan <casey911023@gmail.com>
2025-06-06 06:38:33 -0700

    修改SQL 新增保底計數及卡牌機率

--- user.sql
+++ user.sql
@@ -3,18 +3,22 @@ CREATE TABLE player (
     player_name VARCHAR(20) NOT NULL,
     player_password VARCHAR(30) NOT NULL,
     gacha_stone INT NOT NULL DEFAULT 0,
-    player_money INT NOT NULL DEFAULT 0
+    player_money INT NOT NULL DEFAULT 0,
+    gacha_counter INT DEFAULT 0  --保底計數
 );
 
 CREATE TABLE role (
     role_id INT PRIMARY KEY AUTO_INCREMENT,
-    role_name VARCHAR(30) NOT NULL
+    role_name VARCHAR(30) NOT NULL,
+    role_weight INT NOT NULL, -- 權重決定抽中機率
+    star INT NOT NULL DEFAULT 5
 );
 
 CREATE TABLE player_role (
     player_id CHAR(8) NOT NULL,
     role_id INT NOT NULL,
-    owned BOOLEAN DEFAULT TRUE,
+    owned BOOLEAN DEFAULT FALSE,
+    quantity INT NOT NULL DEFAULT 0,
     PRIMARY KEY (player_id, role_id),
     FOREIGN KEY (player_id) REFERENCES player(player_id),
     FOREIGN KEY (role_id) REFERENCES role(role_id)
@@ -35,9 +39,9 @@ CREATE TABLE player_tool (
 );
 
 -- 預設角色
-INSERT INTO role (role_name) VALUES
-('火焰騎士'), ('冰霜魔導'), ('雷光刺客'), ('森林祭司'),
-('黑暗騎士'), ('光明戰士'), ('風之使者'), ('地獄獸王');
+INSERT INTO role (role_name, role_weight, star) VALUES
+('火焰騎士', 2, 5), ('冰霜魔導', 10, 4), ('雷光刺客', 10, 4), ('森林祭司', 10, 4),
+('黑暗騎士', 30, 3), ('光明戰士', 30, 3), ('風之使者', 30, 3), ('地獄獸王', 30, 3);
 
 -- 預設道具
 INSERT INTO tool (tool_name) VALUES
